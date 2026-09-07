047aa66c8989caf02da1579c12ac54cc0a970010
DaniiilPA <pereslegindanya@gmail.com>
2026-09-07 11:51:46 +0300

    corps

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from endpoints import router 
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from depends import init_db, close_db
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,4 +16,11 @@ async def lifespan(app: FastAPI):
         await close_db()
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
